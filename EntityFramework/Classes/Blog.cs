0fca1677e5b9bace73d35b919dab212193781454
inna.chykova@outlook.com <inna.chykova@outlook.com>
2024-01-29 12:13:29 +0200

    Implement CRUD operations

---
+++ EntityFramework/Classes/Blog.cs
@@ -0,0 +1,11 @@
+﻿namespace EntityFramework.Classes
+{
+    internal class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
